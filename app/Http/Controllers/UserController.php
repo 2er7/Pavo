7c91706b4dcbf164b6f13645bc3c76b48c6a7bca
Ernest E <ee@cnclub.asia>
2018-08-04 17:01:14 +0800

    fix: admin access view lost region error

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,14 +26,14 @@ class UserController extends Controller
     {
         $user = Auth::user();
 
-        if ($user->isAdmin()) {
-            return view('pages.admin.home');
-        }
-
         $data = [
             'serviceName' => trans('service.home'),
             'region' => $this->region,
         ];
+
+        if ($user->isAdmin()) {
+            return view('pages.admin.home')->with($data);
+        }
 
         return view('pages.user.home')->with($data);
     }
